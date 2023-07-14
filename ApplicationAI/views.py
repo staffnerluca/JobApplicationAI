ebc9bd1d9bb747ffaa5dcf635147781206c2aa0c
Luca Staffner <staffnerluca@gmail.com>
2023-07-14 15:53:45 +0200

    Added Downlaoad function

--- ApplicationAI/views.py
+++ ApplicationAI/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+import os
+from django.http import FileResponse
+from django.conf import settings
 
-# Create your views here.
+
 def index(request):
     return render(request, "index.html")
+
+
+def download(request):
+    file_name = "dieKatastrophe.pdf"
+    file_path = os.path.join(settings.BASE_DIR, "ApplicationAI/output", file_name)
+    file = open(file_path, 'rb')
+    response = FileResponse(file, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=JobApplication'
+    return response
