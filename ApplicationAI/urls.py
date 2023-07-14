84dd34bc7ae29fe3ba1d85b53b819fec43eb8f63
Luca Staffner <staffnerluca@gmail.com>
2023-07-14 16:29:02 +0200

    started with Register and Login

--- ApplicationAI/urls.py
+++ ApplicationAI/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
         path("", views.index, name = "index"),
-        path("download", views.download, name = "download")
+        path("download", views.download, name = "download"),
+        path("app", views.app, name = "app"),
+        path("register", views.register, name = "register"),
+        path("login", views.login, name = "login")
         ]
--- ApplicationAI/views.py
+++ ApplicationAI/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import os
-from django.http import FileResponse
+from django.http import FileResponse, HttpResponse
 from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def app(request):
+    return render(request, "app.html")
+
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("app")
+    return render(request, "register.html", {"form": form})
+
+
+def login(request):
+    return HttpResponse("Hello World")
 
 
 def download(request):
